487318ce9890d0205dd077bddcaf54c7c862df4e
Isam Hashmi <isam@techrxservices.com>
2018-08-24 02:43:36 -0500

    Add project files.

---
+++ Business/Models/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Models
+{
+    class Employee
+    {
+        public String FirstName { get; set; }
+
+        public String LastName { get; set; }
+
+        public System.DateTime DOB { get; set; }
+
+        public Guid EmployeeGuid { get; } = Guid.NewGuid();
+
+
+
+        public String Username { get; set; }
+
+        public String UserPassword { get; set; }
+
+    }
+}
---
+++ Business/Program.cs
@@ -0,0 +1,100 @@
+﻿//using Business.Models;
+//using Business.Models;
+using System;
+
+namespace Business
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Business.Models.Employee employee = new Business.Models.Employee(); //Local object small letters but class big letters
+
+            EmployeeSecurityCredentialsEntry(employee);
+
+            Console.ReadKey();
+        }
+
+        public static void EmployeeDOBEntry(Models.Employee employee) //Function should be capital
+        {
+            employee.DOB = new System.DateTime();
+            Console.Write("Year of birth: ");
+            String employeeYear = Console.ReadLine();
+
+            Console.Write("Month of birth: ");
+            String employeeMonth = Console.ReadLine();
+
+            Console.Write("Day of birth: ");
+            String employeeDay = Console.ReadLine();
+
+            int employeeYearInt = Int32.Parse(employeeYear);
+            int employeeMonthInt = Int32.Parse(employeeMonth);
+            int employeeDayInt = Int32.Parse(employeeDay);
+
+            employee.DOB = new System.DateTime(employeeYearInt, employeeMonthInt, employeeDayInt);
+            
+            employee.DOB = employee.DOB.AddHours(10);
+            Console.WriteLine(employee.DOB);
+        }
+
+        public static void EmployeePersonalCredentialsEntry(Models.Employee employee)
+        {
+            Console.WriteLine("Employee ID: " + employee.EmployeeGuid);
+
+            Console.Write("Input First Name: ");
+            employee.FirstName = Console.ReadLine();
+
+            Console.Write("Input Last Name: ");
+            employee.LastName = Console.ReadLine();
+
+            Console.WriteLine(employee.FirstName + " " + employee.LastName); 
+        }
+
+        public static void EmployeeSecurityCredentialsEntry(Models.Employee employee)
+        {
+            bool samePassword = false;
+
+            Console.Write("Please create a new username: ");
+            employee.Username = Console.ReadLine();
+
+            do
+            {
+                Console.Write("Please create a new password: ");
+                String firstPassword = GetHiddenConsoleInput();      //local variable camel casing
+                Console.WriteLine();
+
+                Console.Write("Please reenter a password: ");
+                String secondPassword = GetHiddenConsoleInput();
+                Console.WriteLine();
+                
+
+                if (firstPassword.Equals(secondPassword))
+                {
+                    employee.UserPassword = firstPassword;
+                    samePassword = true;
+                    Console.WriteLine("Security Credentials Entry Success!");
+                }
+                else
+                {
+                    Console.WriteLine("Password did not match. Please try again!");
+                }
+            }
+            while (!samePassword); //Do this while they're not the same password
+
+
+            
+        }
+        private static string GetHiddenConsoleInput()
+        {
+            System.Text.StringBuilder input = new System.Text.StringBuilder();
+            while (true)
+            {
+                var key = Console.ReadKey(true);
+                if (key.Key == ConsoleKey.Enter) break;
+                if (key.Key == ConsoleKey.Backspace && input.Length > 0) input.Remove(input.Length - 1, 1);
+                else if (key.Key != ConsoleKey.Backspace) input.Append(key.KeyChar);
+            }
+            return input.ToString();
+        }
+    }
+}
